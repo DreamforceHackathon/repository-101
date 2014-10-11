6a78f4af9eef1841d3fc043613377aac4eb8a8d2
Henry Shi <henrythe9th@gmail.com>
2014-10-11 12:23:28 -0700

    fixed build

---
+++ CallForce/TwilioClient/Examples/ConferencePhone/Classes/Participant.m
@@ -0,0 +1,19 @@
+//
+//  Copyright 2013 Twilio. All rights reserved.
+//
+ 
+#import "Participant.h"
+
+
+@implementation Participant
+
+@synthesize contactString = _contactString;
+@synthesize type = _type;
+
+-(void)dealloc
+{
+	[_contactString release];
+	[super dealloc];
+}
+
+@end
