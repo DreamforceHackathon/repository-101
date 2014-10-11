787a84b9b738ed5b3f1a1e2846a623c89a9129f8
Henry Shi <henrythe9th@gmail.com>
2014-10-11 13:40:33 -0700

    Added Product VC

---
+++ CallForce/CallForce/ProductViewController.m
@@ -0,0 +1,60 @@
+//
+//  ProductViewController.m
+//  CallForce
+//
+//  Created by henrythe9th on 10/11/14.
+//  Copyright (c) 2014 CallForce. All rights reserved.
+//
+
+#import "ProductViewController.h"
+#import <MDCSwipeToChoose/MDCSwipeToChoose.h>
+#import "Phone.h"
+
+@interface ProductViewController ()
+
+@end
+
+@implementation ProductViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    // You can customize MDCSwipeToChooseView using MDCSwipeToChooseViewOptions.
+    MDCSwipeToChooseViewOptions *options = [MDCSwipeToChooseViewOptions new];
+    options.delegate = self;
+    options.likedText = @"Keep";
+    options.likedColor = [UIColor blueColor];
+    options.nopeText = @"Delete";
+    options.onPan = ^(MDCPanState *state){
+        if (state.thresholdRatio == 1.f && state.direction == MDCSwipeDirectionLeft) {
+            NSLog(@"Let go now to delete the photo!");
+        }
+    };
+    
+    MDCSwipeToChooseView *view = [[MDCSwipeToChooseView alloc] initWithFrame:self.view.bounds
+                                                                     options:options];
+    view.imageView.image = [UIImage imageNamed:@"photo"];
+    [self.view addSubview:view];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - MDCSwipeToChooseDelegate Callbacks
+
+// This is called then a user swipes the view fully left or right.
+- (void)view:(UIView *)view wasChosenWithDirection:(MDCSwipeDirection)direction {
+    if (direction == MDCSwipeDirectionLeft) {
+        NSLog(@"Photo deleted!");
+    } else {
+        NSLog(@"Photo saved!");
+    }
+    
+    Phone *phone = [[Phone alloc] init];
+    [phone connect:@"805-469-5940"];
+    
+}
+
+@end
