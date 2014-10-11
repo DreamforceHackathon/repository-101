e27dfbf27ed3bce0f7f5c72fadac54933416ce48
Zolmeister <zolikahan@gmail.com>
2014-10-11 13:00:41 -0700

    twilio api

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var app = express();
+var twilio = require('twilio');
+
+var TWILIO_SID = process.env.TWILIO_SID
+var TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN
+var TWILIO_APP_SID = process.env.twilio_APP_SID
 
 var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
@@ -8,3 +13,20 @@ var server = app.listen(3000, function() {
 app.get('/', function(req, res){
   res.send('Hello World!');
 });
+
+app.get('/twilioToken', function (req, res) {
+
+  var capability = new twilio.Capability(TWILIO_SID, TWILIO_AUTH_TOKEN);
+
+  //Create a capability token using the TwiML app with sid "AP123", that expires in two minutes
+
+  capability.allowClientIncoming('callForce');
+  capability.allowClientOutgoing(TWILIO_APP_SID);
+  var token = capability.generate();
+  res.json({
+    token: token
+  })
+})
+
+
+module.exports = app
---
+++ Server/test.js
@@ -0,0 +1,22 @@
+var Flare = require('flare-gun')
+var app = require('./server')
+var flare = new Flare().express(app)
+var Joi = require('joi')
+
+describe('test', function () {
+  it('hello', function () {
+    return flare
+      .get('/')
+      .expect(200, 'Hello World!')
+  })
+})
+
+describe('twilio', function () {
+  it('returns a token', function () {
+    return flare
+      .get('/twilioToken')
+      .expect(200, {
+        token: Joi.string()
+      })
+  })
+})
