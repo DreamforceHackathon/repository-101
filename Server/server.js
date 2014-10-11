8043004568045ad30f0197847bb4e853b77f65ad
Zolmeister <zolikahan@gmail.com>
2014-10-11 13:48:20 -0700

    basic calls

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var twilio = require('twilio');
+var bodyParser = require('body-parser')
+var _ = require('lodash')
 
 var TWILIO_SID = process.env.TWILIO_SID
 var TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN
@@ -9,6 +11,8 @@ var TWILIO_APP_SID = process.env.twilio_APP_SID
 var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
 });
+
+app.use(bodyParser.json())
 
 app.get('/', function(req, res){
   res.send('Hello World!');
@@ -28,5 +32,62 @@ app.get('/twilioToken', function (req, res) {
   })
 })
 
+var user  = {
+  products: []
+}
+
+app.post('/users', function (req, res) {
+  user = _.defaults(req.body, user)
+  res.json(user)
+})
+
+app.get('/products', function (req, res) {
+  res.json([
+    {
+      id: 1,
+      name: 'umbrella',
+      company: 'umbrella corp',
+      description: 'The best umbrella ever',
+      leadCount: 20,
+      pricePoint: 15,
+      commission: 0.20
+    },
+    {
+      id: 2,
+      name: 'iphone',
+      company: 'apple',
+      description: 'we\'re watching you',
+      leadCount: 100,
+      pricePoint: 300,
+      commission: 0.05
+    }
+  ])
+})
+
+app.post('/products/:id/accept', function (req, res) {
+  var id = req.params.id
+  user.products.push(id)
+  res.json(user)
+})
+
+app.get('/leads', function (req, res) {
+  res.json([
+    {
+      id: 1,
+      productId: 1,
+      name: 'james',
+      phone: '123-456-7890'
+    }
+  ])
+})
+
+app.post('/leads/:id', function (req, res) {
+  var id = req.params.id
+  var success = req.body.success
+  res.json({
+    id: id,
+    success: success
+  })
+})
 
 module.exports = app
--- Server/test.js
+++ Server/test.js
@@ -23,3 +23,59 @@ describe('twilio', function () {
       })
   })
 })
+
+describe('users', function () {
+  it('creates', function () {
+    return flare
+      .post('/users', {
+        id: 1,
+        data: 123
+      })
+      .expect(200, {
+        id: 1,
+        data: 123,
+        products: Joi.array()
+      })
+  })
+})
+
+describe('products', function () {
+  it('gets all', function () {
+    return flare
+      .get('/products')
+      .expect(200, Joi.array().includes({
+        id: Joi.number(),
+        name: Joi.string(),
+        company: Joi.string(),
+        description: Joi.string(),
+        leadCount: Joi.number(),
+        pricePoint: Joi.number(),
+        commission: Joi.number()
+      }))
+  })
+
+  it('accepts', function () {
+    return flare
+      .post('/products/1/accept')
+      .expect(200)
+  })
+})
+
+describe('leads', function () {
+  it('gets all', function () {
+    return flare
+      .get('/leads')
+      .expect(200, Joi.array().includes({
+        id: Joi.number(),
+        productId: Joi.number(),
+        name: Joi.string(),
+        phone: Joi.string()
+      }))
+  })
+
+  it('posts', function () {
+    return flare
+      .post('/leads/1', {success: true})
+      .expect(200)
+  })
+})
