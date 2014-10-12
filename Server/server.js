068f5cf0748e427342f1af6cb7d2113bb1b18914
Zolmeister <zolikahan@gmail.com>
2014-10-11 17:26:53 -0700

    YEAH SALESFORCE WORKS

--- Server/server.js
+++ Server/server.js
@@ -3,16 +3,51 @@ var app = express();
 var twilio = require('twilio');
 var bodyParser = require('body-parser')
 var _ = require('lodash')
+var nforce = require('nforce')
+var cookieParser = require('cookie-parser')
+var session = require('express-session')
 
 var TWILIO_SID = process.env.TWILIO_SID
 var TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN
 var TWILIO_APP_SID = process.env.twilio_APP_SID
+
+var S_USERNAME = process.env.S_USERNAME
+var S_PASSWORD = process.env.S_PASSWORD
+
+var org = nforce.createConnection({
+  clientId: '3MVG9xOCXq4ID1uHhtUqMuHpGog1YjljbHSCw0abr583NjRe_Wcz5BLxk4m.QTQpLGmbWXDxm6FrJtfFHlN2L',
+  clientSecret: '97071771698148849',
+  redirectUri: 'http://localhost:3000/oauth/_callback',
+  apiVersion: 'v27.0',  // optional, defaults to current salesforce API version
+  environment: 'production',  // optional, salesforce 'sandbox' or 'production', production default
+  mode: 'multi', // optional, 'single' or 'multi' user mode, multi default
+  autoRefresh: true
+});
+
+
+org.authenticate({ username: S_USERNAME, password: S_PASSWORD}, function(err, oauth){
+  // store the oauth object for this user
+  if (err)  return console.log(err);
+
+  var params = ['id', 'description__c', 'number_of_leads__c', 'price__c', 'company__c', 'commission_percent__c', 'name__c', 'photo_url__c' ]
+  org.query({query: 'SELECT ' + params.join(', ') + ' FROM Product2 where price__c != null limit 5', oauth: oauth}, function (err, res) {
+    if (err) return console.log(err);
+    _.forEach(res.records, function (record) {
+      console.log(record._fields);
+    })
+  })
+});
+
 
 var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
 });
 
 app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(session({secret: 'keyboard cat!!!!!!!!!!', resave: true, saveUninitialized: true}))
+app.use(org.expressOAuth({onSuccess: '/home', onError: '/oauth/error'}));  // <--- nforce middleware
+
 
 app.get('/', function(req, res){
   res.send('Hello World!');
